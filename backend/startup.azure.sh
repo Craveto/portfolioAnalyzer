d7863542b5a64498b4a71d27bce4d66980714443
Snehal Dighore <snehaldighore89@gmail.com>
2026-03-12 01:38:39 +0530

    Update startup.azure.sh

--- backend/startup.azure.sh
+++ backend/startup.azure.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 
-echo "Applying migrations..."
+python -m pip install -r requirements.txt
 python manage.py migrate --noinput
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
+gunicorn edaapp.wsgi --bind=0.0.0.0:${PORT:-8000}
 
-echo "Starting Gunicorn..."
-gunicorn edaapp.wsgi --bind=0.0.0.0:${PORT:-8000} --workers=${GUNICORN_WORKERS:-3} --timeout=${GUNICORN_TIMEOUT:-180}
+
+# echo "Applying migrations..."
+# python manage.py migrate --noinput
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# echo "Starting Gunicorn..."
+# gunicorn edaapp.wsgi --bind=0.0.0.0:${PORT:-8000} --workers=${GUNICORN_WORKERS:-3} --timeout=${GUNICORN_TIMEOUT:-180}
